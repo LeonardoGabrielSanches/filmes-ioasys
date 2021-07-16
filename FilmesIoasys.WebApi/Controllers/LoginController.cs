9f78b2ed77181acbf9f8b7e1dd60a5fbc279cf1b
Leonardo Sanches <sleonardogabriel@gmail.com>
2021-07-16 13:52:21 -0300

    Mudança de struct para view model

--- FilmesIoasys.WebApi/Controllers/LoginController.cs
+++ FilmesIoasys.WebApi/Controllers/LoginController.cs
@@ -1,6 +1,4 @@
-using FilmesIoasys.Dominio.Entidades;
 using FilmesIoasys.Dominio.Interfaces.Services;
-using FilmesIoasys.WebApi.Services;
 using FilmesIoasys.WebApi.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -26,23 +24,7 @@ namespace FilmesIoasys.WebApi.Controllers
             if (!usuario.IsValid)
                 return BadRequest(usuario.NotificacaoErro);
 
-            return Ok(Auth.RecuperaUsuarioToken(usuario));
-        }
-    }
-
-    struct Auth
-    {
-        public UsuarioViewModel Usuario { get; set; }
-        public string Token { get; set; }
-
-        public static Auth RecuperaUsuarioToken(Usuario usuario)
-        {
-            var auth = new Auth();
-
-            auth.Usuario = (UsuarioViewModel)usuario;
-            auth.Token = TokenServico.GeraToken(usuario);
-
-            return auth;
+            return Ok(AuthViewModel.RecuperaUsuarioToken(usuario));
         }
     }
 }
