5830de18dc96a84ccfbfb3f9c76d09990ec230c0
Leonardo Sanches <sleonardogabriel@gmail.com>
2021-07-15 23:41:20 -0300

    Serviço de geração do token e mudança na entidade usuario

--- FilmesIoasys.Infra.Data.Sql/Migrations/20210716024009_MudancaTipoAdmin.Designer.cs
+++ FilmesIoasys.Infra.Data.Sql/Migrations/20210716024009_MudancaTipoAdmin.Designer.cs
@@ -4,14 +4,16 @@ using FilmesIoasys.Infra.Data.Sql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FilmesIoasys.Infra.Data.Sql.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210716024009_MudancaTipoAdmin")]
+    partial class MudancaTipoAdmin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
